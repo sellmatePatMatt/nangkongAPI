0c57215f1cb3d26cac78579b171139c89762bd16
Haengseok <godtjrdl98@gmail.com>
2023-04-03 22:18:17 +0900

    파일 네이밍변경

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,18 +1,18 @@
-import "reflect-metadata"
-import { DataSource } from "typeorm"
-import { User } from "./entity/User"
+// import "reflect-metadata"
+// import { DataSource } from "typeorm"
+// import { User } from "./entitys/User"
 
-// TODO: DB암호화
-export const AppDataSource = new DataSource({
-    type: "mysql",
-    host: process.env.DB_HOST || "localhost",
-    port: parseInt(process.env.DB_PORT || "3306"),
-    username: process.env.DB_USER || "root",
-    password: process.env.DB_PASSWORD || "",
-    database: process.env.DB_NAME || "nangkongAPI",
-    synchronize: true,
-    logging: true,
-    entities: [User],
-    migrations: [],
-    subscribers: [],
-})
+// // TODO: DB암호화
+// export const AppDataSource = new DataSource({
+//     type: "mysql",
+//     host: process.env.DB_HOST || "localhost",
+//     port: parseInt(process.env.DB_PORT || "3306"),
+//     username: process.env.DB_USER || "root",
+//     password: process.env.DB_PASSWORD || "",
+//     database: process.env.DB_NAME || "nangkongAPI",
+//     synchronize: true,
+//     logging: true,
+//     entities: [User],
+//     migrations: [],
+//     subscribers: [],
+// })
